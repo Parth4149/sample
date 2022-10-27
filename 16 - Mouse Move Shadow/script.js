59ec15127047d6e3aff62e8840d45a60b18fdebd
Parth Prajapati <pp41495233@gmail.com>
2022-10-27 18:36:44 +0530

    git commit -m"16- Mouse Move Shadow files added"

---
+++ 16 - Mouse Move Shadow/script.js
@@ -0,0 +1,35 @@
+
+const hero = document.querySelector('.hero');
+const text = hero.querySelector('h1');
+const walk = 100; // 100px
+
+function shadow(e) {
+  // const width = hero.offsetWidth;
+  // const height = hero.offsetHeight;
+  const { offsetWidth: width , offsetHeight: height }  = hero; // using destructor
+  let { offsetX: x, offsetY: y} = e;
+  // console.log(x, y);
+
+  if (this !== e.target) {
+    console.log(e.target.offsetLeft, e.target.offsetTop);
+    x = x + e.target.offsetLeft;
+    y = y + e.target.offsetTop;
+  }
+
+  const xWalk = Math.floor((x / width * walk) - (walk / 2));
+  const yWalk = Math.floor((y / height * walk) - (walk / 2)); 
+  
+  text.style.textShadow = `
+  ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7), 
+  ${yWalk}px ${xWalk}px 0 rgba(0,0,255,0.7),
+  ${-xWalk}px ${yWalk}px 0 rgba(0,255,255,0.7),
+  ${xWalk}px ${-yWalk}px 0 rgba(0,255,0,0.7),
+  ${yWalk}px ${-xWalk}px 0 rgba(255,0,0,0.7) 
+  
+  `; // 
+  console.log(xWalk, yWalk);
+}
+
+
+
+hero.addEventListener('mousemove', shadow);
