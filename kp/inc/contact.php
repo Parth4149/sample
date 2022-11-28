9ae9df521e2f47a08890b88aca80ceac2c614997
Parth Prajapati <pp41495233@gmail.com>
2022-11-28 14:37:27 +0530

    project files added

---
+++ kp/inc/contact.php
@@ -0,0 +1,58 @@
+<div class="lower-page" id="contact">
+    <div class="center-container">
+        <!-- center block start -->
+        <div class="center-block contact-bg">
+            <!-- lower content start -->
+            <div class="lower-content">
+                <!-- container start -->
+                <div class="container sections section-custom">
+                    <div class="row">
+
+                        <div class="content-contact col-md-9 ">
+                            <h3 class="text-left text-white contact-heading">
+                                iMe Influencers & Believers:
+                            </h3>
+                            <h6 class="text-center contact-sub-heading">
+                                “pleasing the soul”, NOW & FOREVER <br />
+                                through extraordinary and divine literature, art and media-related storytelling™
+
+                            </h6>
+                            <p class="text-white">
+                                IS on your mind. Whether you are interested in [Kenisha] iDisciple №XXI MIRROR EFFECT
+                                THEORY: Exposing A-YOU within YOU™, looking extraordinary is ordinary™ or partnering
+                                with
+                                KENISHA PARANSO FOUNDATION, we will aid and inspire your transformational journey.
+
+                            </p>
+                            <p class="text-white">
+                                KENISHA PARANSO is committed to provide excellent client experience. You are only a few
+                                clicks away from enjoying our products and your personalized service.
+                            </p>
+                            <form class="container sections contact-form">
+                                <div class="row">
+                                    <div class="col-md-6 px-2">
+                                        <input type="text" name="name" id="name"
+                                            class="contact-input-name contact-input" placeholder="NAME">
+                                    </div>
+                                    <div class="col-md-6 px-2">
+                                        <input type="email" name="email" id="name"
+                                            class=" contact-input-email contact-input" placeholder="E-MAIL">
+                                    </div>
+                                    <div class="col-md-12 px-2">
+                                        <textarea name="message" class="contact-input text-left contact-input-message"
+                                            placeholder="YOUR MESSAGE" id="" cols="30" rows="10"></textarea>
+                                    </div>
+
+                                </div>
+
+                                <button type="submit" class="submit-button-2">SUBMIT</button>
+                            </form>
+
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
