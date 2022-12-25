a1738898d3874ca1236ebedcce4609fbf6199d00
Parth Prajapati <pp41495233@gmail.com>
2022-12-25 12:55:47 +0530

    fix navbar

--- JavaScript/My/neon/public/js/script.js
+++ JavaScript/My/neon/public/js/script.js
@@ -1,19 +1,11 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
-// ********** set date ************
-// const date = document.querySelector('#date');
-// date.innerHTML = new Date().getFullYear(); 
-
-// ********** close links ************
 const navToggle = document.querySelector('.nav-toggle');
 const linksContainer = document.querySelector('.links-container');
 const links = document.querySelector('.links'); // ul
 
 const navbar = document.querySelector('#nav');
 const topLink = document.querySelector('.top-link');
+
+const body = document.querySelector("#body"); // navActive
 
 navToggle.addEventListener('click', (e) => {
   // linksContainer.classList.toggle('show-links');
@@ -47,15 +39,16 @@ scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
+    body.classList.remove('off-nav-is-active');
     // navigate to specific spot
-    const id = e.currentTarget.getAttribute("href").slice(1); // skip # 
+    const id = e.currentTarget.getAttribute("href").slice(1); // skip #
     const element = document.getElementById(id);
-    
+
     const navHeight = navbar.getBoundingClientRect().height;
     const containerHeight = linksContainer.getBoundingClientRect().height;
-    const fixedNav = navbar.classList.contains("fixed-nav");  
-    
-    let position = element.offsetTop - navHeight;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+
+    let position = element.offsetTop - navHeight + 20;
     // console.log(position);
 
     if (!fixedNav) { // home page
@@ -63,7 +56,7 @@ scrollLinks.forEach((link) => {
     }
 
     if (navHeight > 72) { // I need to scroll more
-      position = position + containerHeight; 
+      position = position + containerHeight;
     }
 
     window.scrollTo({
@@ -73,18 +66,3 @@ scrollLinks.forEach((link) => {
     linksContainer.style.height = 0;
   })
 })
-
-
-
-function updateHandle() {
-  this.classList.add("show-project");
-}
-function removeUpdate() {
-  this.classList.remove("show-project");
-}
-
-const projectDiv = document.querySelectorAll(".project-main");
-
-projectDiv.forEach(div => div.addEventListener("mouseover", updateHandle));
-projectDiv.forEach(div => div.addEventListener("mouseout", removeUpdate));
-
