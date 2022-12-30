4ae928d0cb2c2a9fa940337dc2b7acac287bbf65
Parth Prajapati <pp41495233@gmail.com>
2022-12-30 16:49:13 +0530

    toggle nav fixed

--- JavaScript/My/neon/public/js/script.js
+++ JavaScript/My/neon/public/js/script.js
@@ -1,45 +1,48 @@
-const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = document.querySelector('.links-container');
-const links = document.querySelector('.links'); // ul
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links"); // ul
 
-const navbar = document.querySelector('#nav');
-const topLink = document.querySelector('.top-link');
+const navbar = document.querySelector("#nav");
+const topLink = document.querySelector(".top-link");
 
 const body = document.querySelector("#body"); // navActive
 
-navToggle.addEventListener('click', (e) => {
+navToggle.addEventListener("click", (e) => {
   // linksContainer.classList.toggle('show-links');
   const containerHeight = linksContainer.getBoundingClientRect().height;
   const linksHeight = links.getBoundingClientRect().height;
   console.log(containerHeight, linksHeight);
-  linksContainer.style.height = (containerHeight === 0) ? `${linksHeight}px` : 0;
+  // if (containerHeight === 0) {
+  navToggle.classList.toggle("nav-toggle-rotate");
+  // }
+  linksContainer.style.height = containerHeight === 0 ? `${linksHeight}px` : 0;
 });
 
 // ********** fixed navbar ************
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   const scrollHeight = window.pageYOffset;
   const navHeight = navbar.getBoundingClientRect().height;
   if (scrollHeight > navHeight) {
-    navbar.classList.add('fixed-nav');
+    navbar.classList.add("fixed-nav");
   } else {
-    navbar.classList.remove('fixed-nav');
+    navbar.classList.remove("fixed-nav");
   }
 
   if (scrollHeight > 500) {
-    topLink.classList.add('show-link');
+    topLink.classList.add("show-link");
   } else {
-    topLink.classList.remove('show-link');
+    topLink.classList.remove("show-link");
   }
-})
+});
 
 // ********** smooth scroll ************
 // select links
-const scrollLinks = document.querySelectorAll('.scroll-link');
+const scrollLinks = document.querySelectorAll(".scroll-link");
 scrollLinks.forEach((link) => {
   link.addEventListener("click", (e) => {
     // prevent default
     e.preventDefault();
-    body.classList.remove('off-nav-is-active');
+    body.classList.remove("off-nav-is-active");
     // navigate to specific spot
     const id = e.currentTarget.getAttribute("href").slice(1); // skip #
     const element = document.getElementById(id);
@@ -49,20 +52,23 @@ scrollLinks.forEach((link) => {
     const fixedNav = navbar.classList.contains("fixed-nav");
 
     let position = element.offsetTop - navHeight + 20;
-    // console.log(position);
 
-    if (!fixedNav) { // home page
+    navToggle.classList.toggle("nav-toggle-rotate");
+
+    if (!fixedNav) {
+      // home page
       position = position - navHeight;
     }
 
-    if (navHeight > 72) { // I need to scroll more
+    if (navHeight > 72) {
+      // I need to scroll more
       position = position + containerHeight;
     }
 
     window.scrollTo({
-      left:0,
-      top:position,
+      left: 0,
+      top: position,
     });
     linksContainer.style.height = 0;
-  })
-})
+  });
+});
